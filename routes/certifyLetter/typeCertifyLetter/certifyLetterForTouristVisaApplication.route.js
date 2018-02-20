d43bf84fc5bf844a9494a0ddcd25594dbd6c3cfb
sittikiat <miikeoho@gmail.com>
2018-02-20 16:22:55 +0700

    add update join collection

--- routes/certifyLetter/typeCertifyLetter/certifyLetterForTouristVisaApplication.route.js
+++ routes/certifyLetter/typeCertifyLetter/certifyLetterForTouristVisaApplication.route.js
@@ -14,7 +14,7 @@ function getDate() {
     let hour = d.getHours();
     let minute = d.getMinutes();
     let second = d.getSeconds();
-    
+
     if (month.toString().length < 2) month = "0" + month;
     if (day.length < 2) day = "0" + day;
     if (hour.toString().length < 2) hour = "0" + hour;
@@ -30,94 +30,97 @@ router.get("/", (req, res) => {
     res.end("certifyLetterForTouristVisaApplication");
 });
 
-router.post("/add", (req, res) => {
-    MongoClient.connect(process.env.DB_HOSTNAME, (err, client) => {
-        const db = client.db("digitalHR");
-        upload(req, res, (err) => {
-            console.log(req.files)
-            if (req.files === undefined) throw new Error("files is undefined");
-            if (!err) {
-                //-- upload --//
-                const myFiles = [];
-                req.files.forEach((e) => {
-                    let file = {
-                        "filename": e.filename,
-                        "originalname": e.originalname,
-                        "pathFile": path.join(e.destination.split(".").pop(), e.filename),
-                        "size": e.size,
-                        "mimetype": e.mimetype
-                    }
-                    myFiles.push(file);
-                })
-                //-- upload --//
-                let myObj = {
-                    "ticketID": "TID" + Date.now(),
-                    "createdAt": getDate(),
-                    "status": req.body.status,
-                    "typeCertifyLetter": req.body.typeCertifyLetter,
-                    "owner": {
-                        "employeeID": req.body.owner.employeeID || "",
-                        "firstName": req.body.owner.firstName || "",
-                        "lastName": req.body.owner.lastName || "",
-                        "firstNamePassport": req.body.owner.firstNamePassport || "",
-                        "lastNamePassport": req.body.owner.lastNamePassport || "",
-                        "numberOfCopy": req.body.owner.numberOfCopy || "",
-                        "passportNumber": req.body.owner.passportNumber || "",
-                        "passportExpiryDate": req.body.owner.passportExpiryDate || "",
-                        "files": myFiles,
-                        "embassyForVisaApplication": req.body.owner.embassyForVisaApplication || "",
-                        "countryOfVisit": req.body.owner.countryOfVisit || "",
-                        "periodOfVisit": {
-                            "from": req.body.owner.periodOfVisit.from || "",
-                            "to": req.body.owner.periodOfVisit.to || ""
-                        },
-                        "note": req.body.owner.note
-                    }
+router.post("/addFiles", (req, res) => {
+    upload(req, res, (err) => {
+        if (req.files === undefined) throw new Error("files is undefined");
+        if (!err) {
+            //-- upload --//
+            const myFiles = [];
+            req.files.forEach((e) => {
+                let file = {
+                    "filename": e.filename,
+                    "originalname": e.originalname,
+                    "pathFile": path.join(e.destination.split(".").pop(), e.filename),
+                    "size": e.size,
+                    "mimetype": e.mimetype
                 }
-                // let myObj = {
-                //     "ticketID": "TID" + Date.now(),
-                //     "createdAt": getDate(),
-                //     "status": req.body.status,
-                //     "typeCertifyLetter": req.body.typeCertifyLetter,
-                //     "owner": {
-                //         "employeeID": req.body.owner.employeeID,
-                //         "firstName": req.body.owner.firstName,
-                //         "lastName": req.body.owner.lastName,
-                //         "firstNamePassport": req.body.owner.firstNamePassport,
-                //         "lastNamePassport": req.body.owner.lastNamePassport,
-                //         "numberOfCopy": req.body.owner.numberOfCopy,
-                //         "passportNumber": req.body.owner.passportNumber,
-                //         "passportExpiryDate": req.body.owner.passportExpiryDate,
-                //         "files": myFiles,
-                //         "embassyForVisaApplication": req.body.owner.embassyForVisaApplication,
-                //         "countryOfVisit": req.body.owner.countryOfVisit,
-                //         "periodOfVisit": {
-                //             "from": req.body.owner.periodOfVisit.from,
-                //             "to": req.body.owner.periodOfVisit.to
-                //         },
-                //         "note": req.body.owner.note
-                //     }
-                // }
-               
+                myFiles.push(file);
+            })
+            //-- upload --//
 
-                
-                db.collection("certifyLetter").insertOne(myObj, (err, data) => {
+            // connect mongodb
+            MongoClient.connect(process.env.DB_HOSTNAME, (err, client) => {
+                const db = client.db("digitalHR");
+                const fileID = "FID" + Date.now();
+                const myObj = {
+                    "filesID": fileID,
+                    "files": myFiles
+                }
+                db.collection("files").insertOne(myObj, (err, data) => {
                     if (err) throw err;
                     if (data.result.n > 0) {
-                        res.json({ "status": true, "message": `${data.insertedCount} Inserted` });
+                        res.json({ "status": true, "message": "upload file success", "fileID": fileID });
                         console.log("insert complete");
                         client.close();
                     } else {
-                        res.json({ "status": false, "message": `${data.insertedCount} Inserted` });
+                        res.json({ "status": false, "message": "upload file fail" });
                         console.log("insert fail");
                         client.close();
                     }
                 })
+            });
+            // connect mongodb
+
+        } else {
+            throw err;
+            res.end();
+        };
+    });
+
+});
+
+router.post("/add", (req, res) => {
+    MongoClient.connect(process.env.DB_HOSTNAME, (err, client) => {
+        const db = client.db("digitalHR");
+        let myObj = {
+            "ticketID": "TID" + Date.now(),
+            "createdAt": getDate(),
+            "status": req.body.status,
+            "typeCertifyLetter": req.body.typeCertifyLetter,
+            "owner": {
+                "employeeID": req.body.owner.employeeID,
+                "firstName": req.body.owner.firstName,
+                "lastName": req.body.owner.lastName,
+                "firstNamePassport": req.body.owner.firstNamePassport,
+                "lastNamePassport": req.body.owner.lastNamePassport,
+                "numberOfCopy": req.body.owner.numberOfCopy,
+                "passportNumber": req.body.owner.passportNumber,
+                "passportExpiryDate": req.body.owner.passportExpiryDate,
+                "filesID": req.body.owner.filesID,
+                "embassyForVisaApplication": req.body.owner.embassyForVisaApplication,
+                "countryOfVisit": req.body.owner.countryOfVisit,
+                "periodOfVisit": {
+                    "from": req.body.owner.periodOfVisit.from,
+                    "to": req.body.owner.periodOfVisit.to
+                },
+                "note": req.body.owner.note
+            }
+        }
+
+        db.collection("certifyLetter").insertOne(myObj, (err, data) => {
+            if (err) throw err;
+            if (data.result.n > 0) {
+                res.json({ "status": true, "message": `${data.insertedCount} Inserted` });
+                console.log("insert complete");
+                client.close();
             } else {
-                throw err;
-                res.end();
-            };
-        });
+                res.json({ "status": false, "message": `${data.insertedCount} Inserted` });
+                console.log("insert fail");
+                client.close();
+            }
+        })
+
+
 
     })
 });
@@ -126,7 +129,9 @@ router.post("/add", (req, res) => {
 router.get("/show", (req, res) => {
     MongoClient.connect(process.env.DB_HOSTNAME, (err, client) => {
         const db = client.db("digitalHR");
-        db.collection("certifyLetter").find({ "typeCertifyLetter": "certifyLetterForTouristVisaApplication" }).toArray((err, data) => {
+        db.collection("certifyLetter")
+        .aggregate([{ $lookup: { from: "files", localField: "owner.filesID", foreignField: "filesID", as: "owner.filesOwner" }}])
+        .toArray((err, data) => {
             if (err) throw err;
             if (data.length > 0) {
                 res.json({ "status": true, "message": data });
