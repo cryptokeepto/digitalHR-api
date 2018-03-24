99dff973680c1f4a18ce1f0f19c5d7aa8ab756d7
sittikiat <miikeguru@gmail.com>
2018-03-24 16:19:43 +0700

    generate word type employment success

--- routes/word/index.route.js
+++ routes/word/index.route.js
@@ -2,54 +2,50 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const generateEmployment = require("../../templates/word/certifyLetter/typeCertifyLetter/employment");
-let tickets;
-
-function checkType(tickets) {
-    for (const ticket of tickets) {
-        switch (ticket.typeCertifyLetter) {
-            case "employmentCertifyLetter":
-                ticketID = ticket.ticketID;
-                docx = generateEmployment(ticket);
-                
-                break;
-            case "certifyLetterForTouristVisaApplication":
-                break;
-            case "certifyLetterForHousingLoan":
-                break;
-            case "certifyLetterForFurtherEducation":
-                break;
-            case "certifyLetterForBusinessVisaApplication":
-
-                break;
-            default:
-                console.log("typeCertifyLetter not have in system");
-        }
-    }
-}
+let ticketID;
+var docx;
 
 router.get("/", (req, res) => {
     res.end("word");
 })
 
-router.get("/download", (req, res) => {
-    console.log(tickets);
-    // res.writeHead(200, {
-    //     "Content-Type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    //     'Content-disposition': `attachment; filename=employment-${ticketID}.docx`
-    // })
-    // docx.generate(res, {
-    //     "finalize": function (writen) {
-    //         console.log("Writed success: " + writen);
-    //     },
-    //     "error": function (err) {
-    //         console.log("error" + err)
-    //     }
-    // });
-});
+router.route("/download")
+    .post((req, res) => {
+        const tickets = req.body;
 
-router.post("/download", (req, res) => {
-    console.log("ok")
-    // tickets = req.body;
-})
+        for (const ticket of tickets) {
+            switch (ticket.typeCertifyLetter) {
+                case "employmentCertifyLetter":
+                    // ยังขาดแบ่งประเถทของ employment
+                    ticketID = ticket.ticketID;
+                    docx = generateEmployment(ticket);
+                    break;
+                case "certifyLetterForTouristVisaApplication":
+                    break;
+                case "certifyLetterForHousingLoan":
+                    break;
+                case "certifyLetterForFurtherEducation":
+                    break;
+                case "certifyLetterForBusinessVisaApplication":
+                    break;
+                default:
+                    console.log("typeCertifyLetter not have in system");
+            }
+        }
+        res.end();
+    })
+    .get((req, res) => {
+        if (docx === undefined || ticketID === undefined) throw "docx and ticketID is not value, Please generate word first time";
+        res.writeHead(200, {
+            "Content-Type": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            'Content-disposition': `attachment; filename=${ticketID}.docx`
+        });
+        docx.generate(res, {
+            "finalize": () => console.log("Writed success"),
+            "error": () => { throw err }
+        });
+    })
+
+
 
 module.exports = router;
