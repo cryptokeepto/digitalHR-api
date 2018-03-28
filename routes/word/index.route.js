ed3b97cef14eae4bf1f1e66407082a7c13ab9ac5
Sittikiat Sujitranon <sittikiat.sujitranon@thomsonreuters.com>
2018-03-28 16:49:59 +0700

    generate word await

--- routes/word/index.route.js
+++ routes/word/index.route.js
@@ -2,8 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const Zip = require("node-7z");
+const rimraf = require("rimraf"); // module delete directory same rm -rf
 let ticketID;
 var docx;
+
 // typeWord
 const generateEmployment = require("../../templates/word/certifyLetter/typeCertifyLetter/employment");
 const generateEmploymentAndSalary = require("../../templates/word/certifyLetter/typeCertifyLetter/employmentAndSalary");
@@ -19,32 +22,54 @@ const generateLHBank = require("../../templates/word/certifyLetter/typeCertifyLe
 const generateUOB = require("../../templates/word/certifyLetter/typeCertifyLetter/banks/UOB");
 // typeWord
 
+
 function checkFilesDir(filePath) {
     let checkFiles = fs.existsSync(filePath);
-    if (checkFiles !== true) 
+    if (checkFiles !== true) {
         fs.mkdirSync(filePath);
+    }
 }
 
+function compressFileToZip(ticketID, file) {
+    const zip = new Zip();
+    const option = {
+        t: "zip"
+    };
+    zip.add(`./files/generateDocuments/word/${ticketID}`, file, option)
+        .then(() => {
+            rimraf(`./files/generateDocuments/word/${ticketID}`, function (err) {
+                if (err) throw err;
+                console.log(`generate ${ticketID}.zip and delete old folder success`);
+            });
+        })
+        .catch((error) => {
+            console.log(`generate ${ticketID}.zip and delete old folder fail`);
+            throw err;
+        })
+}
 
-function checkBanks(ticket, banks) {
-    // check dir is have if not have dir system automate generate dir
+function checkBanksAndGenerate(ticket, banks) {
+    // check directory is have if not have directory system automate generate directory
     checkFilesDir("./files");
     checkFilesDir("./files/generateDocuments");
     checkFilesDir("./files/generateDocuments/word");
+    checkFilesDir(`./files/generateDocuments/word/${ticket.ticketID}`);
 
     if (banks.BBL) {
-        generateBBL(ticket).generate(fs.createWriteStream(`./files/generateDocuments/word/${ticket.ticketID}-BBL.docx`))
+        generateBBL(ticket).generate(fs.createWriteStream(`./files/generateDocuments/word/${ticket.ticketID}/${ticket.ticketID}-BBL.docx`))
     }
     if (banks.GHB) {
-        generateGHB(ticket).generate(fs.createWriteStream(`./files/generateDocuments/word/${ticket.ticketID}-GHB.docx`));
+        generateGHB(ticket).generate(fs.createWriteStream(`./files/generateDocuments/word/${ticket.ticketID}/${ticket.ticketID}-GHB.docx`));
     }
     if (banks.LHBank) {
-        generateLHBank(ticket).generate(fs.createWriteStream(`./files/generateDocuments/word/${ticket.ticketID}-LHBank.docx`));
+        generateLHBank(ticket).generate(fs.createWriteStream(`./files/generateDocuments/word/${ticket.ticketID}/${ticket.ticketID}-LHBank.docx`));
     }
     if (banks.UOB) {
-        generateUOB(ticket).generate(fs.createWriteStream(`./files/generateDocuments/word/${ticket.ticketID}-UOB.docx`));
+        generateUOB(ticket).generate(fs.createWriteStream(`./files/generateDocuments/word/${ticket.ticketID}/${ticket.ticketID}-UOB.docx`));
     }
 }
+
+
 
 router.get("/", (req, res) => {
     res.end("word");
@@ -68,14 +93,16 @@ router.route("/download")
                 break;
             case "certifyLetterForHousingLoan":
                 ticketID = ticket.ticketID;
-                checkBanks(ticket, ticket.owner.banks);
+                checkBanksAndGenerate(ticket, ticket.owner.banks);
+                setTimeout(() => {
+                    compressFileToZip(ticketID, `./files/generateDocuments/word/${ticketID}`);
+                }, 1000)
                 break;
             case "certifyLetterForBusinessVisaApplication":
                 break;
             default:
                 console.log("typeCertifyLetter not have in system");
         }
-
         res.end();
     })
     .get((req, res) => {
